0ab9f9d430c6ddee158150a02dfb3048bfe38be2
Taufik Akbar Maliki <taufikakbarmalikitkj@gmail.com>
2023-03-22 12:30:03 +0800

    woke

---
+++ precache-manifest.932bc8a593dfb7770a4049d97a1e0704.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6bf35af64ca7369a6fa6b0849643bf5",
+    "url": "404.html"
+  },
+  {
+    "revision": "956abc59524946e0b3af0f153550540f",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "10b31504e302337e05d08819c10b85a6",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "ea045679115b3db698ba",
+    "url": "css/app~292b85a0.05b2d6a4.css"
+  },
+  {
+    "revision": "209e09289e0224dffdb7",
+    "url": "css/app~d0ae3f07.c0e1be7a.css"
+  },
+  {
+    "revision": "e839493b1ce83ae34d6e",
+    "url": "css/page-about~bccdb9b9.d691dc90.css"
+  },
+  {
+    "revision": "a0fc6c24d1f3ff9ac281887c92b24acd",
+    "url": "fastclick.min.js"
+  },
+  {
+    "revision": "7e1c5e669eb776c4bdc7e949f3e78471",
+    "url": "firebase-messaging-sw.js"
+  },
+  {
+    "revision": "29f9630f7d87a79830d1c321e1600f2e",
+    "url": "fonts/typicons.29f9630f.ttf"
+  },
+  {
+    "revision": "95aa28e29618c068e8a53f64c87cb6a9",
+    "url": "fonts/typicons.95aa28e2.woff"
+  },
+  {
+    "revision": "a509074c88f07e40a8d2f158c4fcdda8",
+    "url": "fonts/typicons.a509074c.eot"
+  },
+  {
+    "revision": "956abc59524946e0b3af0f153550540f",
+    "url": "img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "10b31504e302337e05d08819c10b85a6",
+    "url": "img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "cae8fddf5efaa40d4b30d3ec57f7d979",
+    "url": "img/typicons.cae8fddf.svg"
+  },
+  {
+    "revision": "0ad48cf595276a9edd90b3e56eb3bccc",
+    "url": "index.html"
+  },
+  {
+    "revision": "ea045679115b3db698ba",
+    "url": "js/app~292b85a0.f70cc0bc.js"
+  },
+  {
+    "revision": "209e09289e0224dffdb7",
+    "url": "js/app~d0ae3f07.b124148d.js"
+  },
+  {
+    "revision": "f36c0c390f3fb69dccc4",
+    "url": "js/chunk-2d0e17d2.e5be458c.js"
+  },
+  {
+    "revision": "2006436243cc2b8acf53",
+    "url": "js/chunk-vendors~d939e436.422dceac.js"
+  },
+  {
+    "revision": "b2ed455c00d171ba1e42",
+    "url": "js/chunk-vendors~daa565d3.fa408697.js"
+  },
+  {
+    "revision": "e839493b1ce83ae34d6e",
+    "url": "js/page-about~bccdb9b9.8bdbcf95.js"
+  },
+  {
+    "revision": "a33f974cd4f64abc3300",
+    "url": "js/page-about~page-index~253ae210.a16f36be.js"
+  },
+  {
+    "revision": "821ddf242cb9cf56731d",
+    "url": "js/page-about~page-index~88d7667b.d3f3d3bf.js"
+  },
+  {
+    "revision": "b1904a8b2354ce155018",
+    "url": "js/page-index~31ecd969.df6714ed.js"
+  },
+  {
+    "revision": "47047fee26557b55d985952bdfc6cba1",
+    "url": "model/face_landmark_68_tiny_model.bin"
+  },
+  {
+    "revision": "b3de953de785a39831af6c0cbff5e4f9",
+    "url": "model/face_recognition_model.bin"
+  },
+  {
+    "revision": "2e48b20953b0c59df47459d0319843a0",
+    "url": "model/tiny_face_detector_model.bin"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+]);
